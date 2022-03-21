b22de22d0e0f6bc7b59248cf058c8a719cb780ea
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2022-03-21 20:21:54 -0300

    styled components starts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import styled from "styled-components";
 
+const Title = styled.h1`
+  font-size: 80px;
+  color: #8257e6;
+`;
 
 export function App() {
   return (
     <div className="App">
-     
+      <Title>Hello world</Title>
     </div>
   );
 }
-
-
